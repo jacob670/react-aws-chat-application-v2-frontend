e395e6d8c3f49e19354cb84efd3e638d5ee719a1
Jacob Esteves <esteves.jacob.c@gmail.com>
2024-09-08 15:10:03 -0400

    trying toa add the rec feature but some issues -backend calling is weird, but once fixed then lambda function will use its id to get recommendations yea

--- src/components/RecommendedMovie.js
+++ src/components/RecommendedMovie.js
@@ -4,8 +4,44 @@ import './css/AuthenticatedPages/RecommendedMovies.css';
 import { json, Link, useLocation, useNavigate } from 'react-router-dom';
 
 const RecommendedMovie = () => {
+    const [movieID, setMovieID] = useState('');
+    const [mes, setMes] = useState('');
+
+    const handleChange = (e) => {
+        setMovieID(e.target.value);
+      };
+    
+
+    const handleSearch = async (e) => {
+        setMovieID(e.target.value);
+
+        try {
+            const response = await axios.post('http://localhost:8083/api/movie/fetchMovieId', { queryString: movieID });
+            console.log(response.data);
+          } catch (error) {
+            console.error('Error fetching movie data:', error);
+          }
+      };
+    
+
     return (
-        <p>Hello rec</p>
+        <div class='rec-container'>
+
+            <div class='header'>
+                <Link to="/quickChats" class='trending-movie-title-link'>
+                    <h1>Movie Recommendations!</h1>
+                </Link>
+            </div>
+
+            <div class='dir'>
+                <p>Just enter a movie that is similar to what you want to watch!</p>
+                <input class="input" name="text" placeholder="Search..." type="search" value={movieID} onChange={handleChange}/>
+
+                <button type="submit" onClick={handleSearch} >Search For Recommendations!</button>
+            </div>
+
+        </div>
+
     );
 }
 
